3165c2555385a7a512be008fac51b4a761b27fbe
nargok <nargok@gmail.com>
2021-09-13 20:37:56 +0900

    docker setting for elasticsearch

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,14 @@
 class Article < ApplicationRecord
   include Elasticsearch::Model
 
+  settings do
+    mapping dynamic: 'false' do # 動的にマッピングを生成しない
+      indexes :title, analyzer: 'kuromoji', type: 'text'
+      indexes :author, analyzer: 'kuromoji', type: 'text'
+    end
+  end
+
+
   def self.search_author(keyword)
     if keyword.present?
       query = {
