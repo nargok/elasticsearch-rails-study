f74bddfaa3710c67c25be9e5ce0964214809b2f6
nargok <nargok@gmail.com>
2021-09-13 19:24:33 +0900

    search author method

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,19 @@
 class Article < ApplicationRecord
   include Elasticsearch::Model
+
+  def self.search_author(keyword)
+    if keyword.present?
+      query = {
+        "query": {
+          "match": {
+            "author": keyword
+          }
+        }
+      }
+
+      self.__elasticsearch__.search(query)
+    else
+      self.none
+    end
+  end
 end
